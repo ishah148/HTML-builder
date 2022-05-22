90a19dc2d183e86f6cfdbd851ad77ae935f74b1a
Igor Shah <zombee1992@gmail.com>
2022-05-22 23:44:14 +0300

    task 3 finish

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,9 +1,15 @@
 /* eslint-disable*/
-const fs = require('fs');
-const path = require('path')
-fs.readdir('03-files-in-folder/secret-folder',(err,data)=>{
-    console.dir(data)
-    console.log(path.extname(data[0]))
-    console.log(fs.statSync('03-files-in-folder/secret-folder/' + data[0]).size)
-    
-})
+const fs = require('fs/promises');
+const path = require('path');
+
+readFiles()
+async function readFiles() {
+    const currentFolder = '03-files-in-folder/secret-folder/';
+    const a1 = await fs.readdir(currentFolder, { withFileTypes: true })
+    for (file of a1) {
+        const size = (await fs.stat(currentFolder + file.name)).size
+        if (file.isFile()) {
+            console.log(file.name + '\t\t', path.extname(file.name) + '\t\t', size + '\t\t')
+        }
+    }
+}
