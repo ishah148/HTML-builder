8584d0110eccba7c4f692a7e978388c739a03890
Igor Shah <zombee1992@gmail.com>
2022-05-22 23:53:37 +0300

    task 3 finish fix

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,9 +7,9 @@ async function readFiles() {
     const currentFolder = '03-files-in-folder/secret-folder/';
     const a1 = await fs.readdir(currentFolder, { withFileTypes: true })
     for (file of a1) {
-        const size = (await fs.stat(currentFolder + file.name)).size
+        const size = (await fs.stat(currentFolder + file.name)).size/1000 + 'kB'
         if (file.isFile()) {
-            console.log(file.name + '\t\t', path.extname(file.name) + '\t\t', size + '\t\t')
+            console.log(file.name + '\t-', path.extname(file.name) + '\t-', size)
         }
     }
 }
