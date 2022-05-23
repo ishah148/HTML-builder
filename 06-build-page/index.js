de052ada21f662b9e89ee2c09869dd0721d3bde0
Igor Shah <zombee1992@gmail.com>
2022-05-24 00:11:05 +0300

    06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,51 +1,47 @@
 /* eslint-disable*/
+const { rmSync } = require('fs');
 const fs = require('fs/promises');
 const path = require('path');
+const { formatWithOptions } = require('util');
 console.log(__dirname)
 fs.mkdir(__dirname + '/project-dist', { recursive: true })
-// copyDir(__dirname + '/assets', __dirname + '/project-dist')
+createBundle(__dirname + '/' + 'styles')
+editHtml(__dirname)
+copyAssets(__dirname + '/assets/')
 
 
-// createBundle(__dirname + '/' + 'styles')
+async function editHtml(fd){
+    const template  = await fs.readFile(fd +'/template.html','utf-8')
+    const header  = await fs.readFile(fd +'/components/header.html','utf-8')
+    const articles  = await fs.readFile(fd +'/components/articles.html','utf-8')
+    const footer =  await fs.readFile(fd +'/components/footer.html','utf-8')
+    let result = template.replace(/{{header}}/,header)
+    result = result.replace(/{{articles}}/,articles)
+    result = result.replace(/{{footer}}/,footer)
+    await fs.writeFile(__dirname + '/project-dist/index.html',result)
+    console.log('html done!')
+}
+
 async function createBundle(folder) {
     const files = await fs.readdir(folder, { withFileTypes: true })
-    fs.writeFile(__dirname + '/project-dist/bundle.css', '')
+    fs.writeFile(__dirname + '/project-dist/style.css', '')
     for (file of files) {
         if (file.isFile() && path.extname(file.name) === '.css') {
             const content = await fs.readFile(folder + '/' + file.name, 'utf-8')
-            fs.appendFile(__dirname + '/project-dist/bundle.css', content + '\n\n')
+            fs.appendFile(__dirname + '/project-dist/style.css', content + '\n\n')
         }
     }
+    console.log('bundle created')
 }
 
-
-// copyDir(__dirname + '/files',__dirname + '/files-copy')
-async function copyDir(firstPath, secondPath) {
-    const copying = await fs.mkdir(secondPath, { recursive: true })
-    const files = await readFiles(firstPath)
-    for(let file of files){
-        fs.copyFile(firstPath + '/' + file.name, secondPath + '/' + file.name) 
-    }
-
-}
-
-// async function readFiles(folder,show = false) {
-//     const files = await fs.readdir(folder, { withFileTypes: true })
-//     return files
-// }
-copyAssets(__dirname + '/assets/')
 async function copyAssets(fd) {
     const folders = ['fonts/','img/','svg/']
     for(let folder of folders){
-        fs.mkdir(__dirname + '/project-dist/' + folder, { recursive: true })
+       await fs.mkdir(__dirname + '/project-dist/assets/' + folder, { recursive: true })
         const files = await fs.readdir(fd + folder, { withFileTypes: true })
         for (let file of files) {
-            fs.copyFile(fd + '/' + folder + '/' + file.name, __dirname + '/project-dist/' + folder + '/' + file.name) 
-            if (file.isFile()) {   
-            }
+            await fs.copyFile(fd + '/' + folder + '/' + file.name, __dirname + '/project-dist/assets/' + folder + '/' + file.name) 
         }
     }
-    return new Promise(resolve =>{
-        resolve('finish')
-    })
+    console.log('assets copied')
 }
