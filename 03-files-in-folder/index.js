d31db94f275fa73903a88f3fcf5f1df1e9a7f158
Igor Shah <zombee1992@gmail.com>
2022-05-23 18:20:01 +0300

    04-copy-directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,14 +2,16 @@
 const fs = require('fs/promises');
 const path = require('path');
 
-readFiles()
-async function readFiles() {
+readFiles('03-files-in-folder/secret-folder/')
+async function readFiles(folder) {
     const currentFolder = '03-files-in-folder/secret-folder/';
-    const a1 = await fs.readdir(currentFolder, { withFileTypes: true })
-    for (file of a1) {
-        const size = (await fs.stat(currentFolder + file.name)).size/1000 + 'kB'
+    const files = await fs.readdir(folder, { withFileTypes: true })
+    for (file of files) {
+        const size = (await fs.stat(folder + file.name)).size/1000 + 'kB'
         if (file.isFile()) {
             console.log(file.name + '\t-', path.extname(file.name) + '\t-', size)
         }
     }
+    return files
 }
+
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+/* eslint-disable*/
+const fs = require('fs/promises');
+const path = require('path');
 
+copyDir(__dirname + '/files',__dirname + '/files-copy')
+async function copyDir(firstPath, secondPath) {
+    const copying = await fs.mkdir(secondPath, { recursive: true })
+    const files = await readFiles(firstPath)
+    for(let file of files){
+        fs.copyFile(firstPath + '/' + file.name, secondPath + '/' + file.name) 
+    }
+}
+
+async function readFiles(folder,show = false) {
+    const files = await fs.readdir(folder, { withFileTypes: true })
+    return files
+}
