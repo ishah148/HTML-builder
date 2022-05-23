ec4020a64051b4ef3158e47bd359e8fee2b0aee3
Igor Shah <zombee1992@gmail.com>
2022-05-24 00:36:44 +0300

    refactor: 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,25 +1,22 @@
 /* eslint-disable*/
-const { rmSync } = require('fs');
 const fs = require('fs/promises');
 const path = require('path');
-const { formatWithOptions } = require('util');
-console.log(__dirname)
 fs.mkdir(__dirname + '/project-dist', { recursive: true })
 createBundle(__dirname + '/' + 'styles')
 editHtml(__dirname)
 copyAssets(__dirname + '/assets/')
 
-
-async function editHtml(fd){
-    const template  = await fs.readFile(fd +'/template.html','utf-8')
-    const header  = await fs.readFile(fd +'/components/header.html','utf-8')
-    const articles  = await fs.readFile(fd +'/components/articles.html','utf-8')
-    const footer =  await fs.readFile(fd +'/components/footer.html','utf-8')
-    let result = template.replace(/{{header}}/,header)
-    result = result.replace(/{{articles}}/,articles)
-    result = result.replace(/{{footer}}/,footer)
-    await fs.writeFile(__dirname + '/project-dist/index.html',result)
-    console.log('html done!')
+async function editHtml(fd) {
+    let template = await fs.readFile(fd + '/template.html', 'utf-8')
+    const files = await fs.readdir(fd + '/components/', { withFileTypes: true })
+    for(let file of files){
+        const fileName = file.name.split('.')[0]
+        const elem =  await fs.readFile(fd + `/components/${fileName}.html`, 'utf-8')
+        const reg = new RegExp(`{{${fileName}}}`)
+        template = template.replace(reg, elem)
+    }
+    await fs.writeFile(__dirname + '/project-dist/index.html', template)
+    console.log('html built !')
 }
 
 async function createBundle(folder) {
@@ -31,17 +28,17 @@ async function createBundle(folder) {
             fs.appendFile(__dirname + '/project-dist/style.css', content + '\n\n')
         }
     }
-    console.log('bundle created')
+    console.log('bundle created !')
 }
 
 async function copyAssets(fd) {
-    const folders = ['fonts/','img/','svg/']
-    for(let folder of folders){
-       await fs.mkdir(__dirname + '/project-dist/assets/' + folder, { recursive: true })
+    const folders = ['fonts/', 'img/', 'svg/']
+    for (let folder of folders) {
+        await fs.mkdir(__dirname + '/project-dist/assets/' + folder, { recursive: true })
         const files = await fs.readdir(fd + folder, { withFileTypes: true })
         for (let file of files) {
-            await fs.copyFile(fd + '/' + folder + '/' + file.name, __dirname + '/project-dist/assets/' + folder + '/' + file.name) 
+            await fs.copyFile(fd + '/' + folder + '/' + file.name, __dirname + '/project-dist/assets/' + folder + '/' + file.name)
         }
     }
-    console.log('assets copied')
+    console.log('assets copied !')
 }
