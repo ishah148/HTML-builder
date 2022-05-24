84388fc47b00bcda94b887dd86c36e8cab5f9729
Igor Shah <zombee1992@gmail.com>
2022-05-24 23:32:45 +0300

    fix: 01 02

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,26 @@
 
+// fs.readFile(taskPath, 'utf-8', (err, data) => {
+//     console.log(data);
+// });
 /* eslint-disable*/
 const fs = require('fs');
-fs.readFile('01-read-file/text.txt', 'utf-8', (err, data) => {
-    console.log(data);
-});
+const path = require('path')
+const taskPath = path.join(__dirname,'text.txt')
+const stream = fs.createReadStream(taskPath, 'utf-8');
+let text = '';
 
+stream.on('readable',()=>{
+    text = stream.read()
+    if(text!== null) console.log(text)
+})
+
+stream.on('end',()=>{
+    console.log('')
+})
+
+stream.on('error',()=>{
+    console.log('error')
+})
 // При наличии каких-либо ошибок прошу связяться со мной telegram: @IgorTg123 discord: IgorShah(@ishah148)#3091
+
+
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,15 +1,24 @@
 /* eslint-disable*/
 const fs = require('fs');
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
-const rl = readline.createInterface({ input, output });
-rl.question('Please, input text? ', (answer) => {
-    fs.writeFile('02-write-file/text.txt', answer, (err, data) => {
-        if(err){
-            console.log(err)
-        }
-    });
-    console.log(`You entered : ${answer}`);
-    rl.close();
+const path = require('path');
+const { stdout, stdin } = require('process');
+const myPath = path.join(__dirname, 'text.txt');
+const wrStream = fs.createWriteStream(myPath);
+
+stdout.write('enter text, please! \n')
+
+stdin.on('data', (data) => {
+    if (data.toString().trim() === 'exit') {
+        console.log('Done!')
+        process.exit();
+    }
+    wrStream.write(data.toString());
 });
+
+process.on('SIGINT', () => {
+    console.log('Done!')
+    process.exit();
+});
+
+
 // При наличии каких-либо ошибок прошу связяться со мной telegram: @IgorTg123 discord: IgorShah(@ishah148)#3091
